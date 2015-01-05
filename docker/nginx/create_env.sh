3409bdb4b60fca0cb10d7343870a1553fc07e05b
Jose Ibañez <ibanez.j@gmail.com>
2015-01-05 12:21:04 +0000

    Services

--- docker/nginx/create_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-touch /etc/enviroment
-COREOS_PRIVATE_IPV4=`/bin/ifconfig eth0 | grep "inet " | awk  '{print $2}'`
-echo "COREOS_PRIVATE_IPV4" $COREOS_PRIVATE_IPV4  | tee /etc/enviroment
-
-
