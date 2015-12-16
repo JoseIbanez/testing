53f595177c5658bab12ab9cddc367ceec152a9db
José Ibañez Vela <ibanez@Martes.local>
2015-12-16 18:46:11 +0100

    test

--- flask/proj02/proj02.py
+++ flask/proj02/proj02.py
@@ -1,10 +1,12 @@
 from flask import Flask
 application = Flask(__name__)
+cuenta=0
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    global cuenta
+    cuenta=cuenta+1
+    return "<h1 style='color:blue'>Hello There! c:"+str(cuenta)+"</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
-
