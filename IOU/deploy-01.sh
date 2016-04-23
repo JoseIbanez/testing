3d71d98e759b2c272eb3d81269e1f2a5aedc0973
José Ibañez <ibanez.j@gmail.com>
2016-04-23 21:45:51 +0000

    IOU on aws

---
+++ IOU/deploy-01.sh
@@ -0,0 +1,27 @@
+apt-get install joe
+
+
+#==================================================
+echo "Configure Additional Repo"
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+#add-apt-repository "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse"
+sudo dpkg --add-architecture i386
+
+apt-get update -y
+
+echo "Installing deps"
+sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0
+sudo apt-get install -y libssl1.0.0:i386
+
+echo "Post configuration"
+sudo ln -s /lib/i386-linux-gnu/libcrypto.so.1.0.0 /lib/libcrypto.so.4
+
+aws s3 sync s3://fibratel.es/utils/xxx $IOU/
+
+
+echo "Generate license file"
+$IOU/scripts/keygen.py | grep -e 'lic' -e '=' > ~/.iourc
+
+
+echo "Restarting"
+#sudo service mongod restart
---
+++ IOU/lab1/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IOU="~/IOU"
+
+for n in {1..5}
+do
+echo $n
+port=$((2000 + $n))
+echo $IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l3-adventerprisek9-15.4.1T.bin -p $port -- -e2 -s0 $n
+sleep 5
+done
+
