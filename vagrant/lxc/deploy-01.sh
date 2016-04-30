36790b761cb0fa58d895fcfc84dd779855432034
José Ibañez <ibanez.j@gmail.com>
2016-04-30 17:52:01 +0200

    aws

--- vagrant/lxc/deploy-01.sh
+++ vagrant/lxc/deploy-01.sh
@@ -13,11 +13,18 @@ sudo apt-get update -y
 
 echo "Installing deps"
 sudo apt-get install -y lxd
+sudo apt-get install -y unzip
 sudo lxd init --auto
+
 
 echo "Post configuration"
 #cp /vagrant/config/mongod.conf /etc/
 cp -a /vagrant/scripts ./
+
+wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
+unzip ./consul_0.6.4_linux_amd64.zip
+mkdir ./bin/
+sudo mv consul ./bin/
 
 
 echo "Restarting"
