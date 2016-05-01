c3d945d661d5faa9ee54f204e7a8050926d98703
José Ibañez <ibanez.j@gmail.com>
2016-05-01 09:01:56 +0000

    dd

--- vagrant/lxc/deploy-01.sh
+++ vagrant/lxc/deploy-01.sh
@@ -1,6 +1,6 @@
 #==================================
 #Some configuration for S.O.
-apt-get install joe
+sudo apt-get install joe
 cp /vagrant/config/locale /etc/default/locale
 
 #==================================================
@@ -8,7 +8,7 @@ echo "Configure Additional Repo"
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 #add-apt-repository "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse"
 
-sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable 
+sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
 sudo apt-get update -y
 
 echo "Installing deps"
@@ -21,11 +21,13 @@ echo "Post configuration"
 #cp /vagrant/config/mongod.conf /etc/
 cp -a /vagrant/scripts ./
 
-wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
-unzip ./consul_0.6.4_linux_amd64.zip
-mkdir ./bin/
-sudo mv consul ./bin/
-
+if [ ! -f ./consul_0.6.4_linux_amd64.zip ]
+then
+	wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
+	unzip ./consul_0.6.4_linux_amd64.zip
+	mkdir ./bin/
+	sudo mv consul ./bin/
+fi
 
 echo "Restarting"
 #sudo service mongod restart
