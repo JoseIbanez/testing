2cd139ba7983497aae5fededc7ea69674e846379
José Ibañez <ibanez.j@gmail.com>
2016-05-17 00:10:49 +0200

    compose

--- docker/proj04/worker/p06.py
+++ docker/proj04/worker/p06.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import redis
+import time
 
 with open("redis.lst","r") as f:
   content=f.read().splitlines()
@@ -11,9 +12,11 @@ r_server = redis.Redis(content[0])
 r_server.set("name", "DeGizmo")
 n=r_server.get("name")
 
-r_server.incr("hit_counter")
-c=r_server.get("hit_counter")
+while True:
+    r_server.incr("hit_counter")
+    c=r_server.get("hit_counter")
 
-print n
-print c
+    print n
+    print c
 
+    time.sleep(1)
