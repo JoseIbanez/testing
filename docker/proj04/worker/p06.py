aa7ae4d6755b6dd3fd222a2154f23558f9678221
José Ibañez <ibanez.j@gmail.com>
2016-05-22 21:49:58 +0200

    flask

--- docker/proj04/worker/p06.py
+++ docker/proj04/worker/p06.py
@@ -18,10 +18,12 @@ while True:
     r_server.expire("worker:"+mac,10)
     print "all",c
     print mac,wc
+
     for w in r_server.smembers("workers"):
         wc=r_server.hget(w,"hit")
         if (wc is None):
             r_server.srem("workers",w)
             continue
         print w,wc
+
     time.sleep(1)
--- docker/proj04/worker/wsgi.py
+++ docker/proj04/worker/wsgi.py
@@ -1,4 +1,4 @@
-from proj04 import app
+from p07 import app
 
 if __name__ == "__main__":
     app.run()
