1452b6e67563e28dc31785fd2a5c50d4c65293fa
José Ibañez <ibanez.j@gmail.com>
2016-07-20 20:34:40 +0200

    Docker v1.12

--- docker/proj05/docker-machine/start-aws.sh
+++ docker/proj05/docker-machine/start-aws.sh
@@ -18,23 +18,47 @@ docker-machine create \
   --amazonec2-spot-price 0.10 \
   --amazonec2-region eu-central-1 \
   --amazonec2-instance-type m3.large \
-  dck-03 &
+  dck-01 &
 
 
 
 #####
 
 docker swarm init --secret 4wh0m5k61ba8ejezjrccnc3pf \
-  --listen-addr 172.31.25.100
+  --listen-addr 172.31.25.104
 
 docker swarm join --secret 4wh0m5k61ba8ejezjrccnc3pf \
-  172.31.25.100:2377
+  172.31.25.104:2377
 
+docker node promote dck-02
 
 ########
+docker network create -d overlay \
+    --subnet=172.16.10.0/24 \
+    --gateway=172.16.10.1 \
+    my-net-01
 
-docker service create --replicas 1 --name helloworld alpine ping docker.com
 
+docker service create --replicas 1 \
+    --network my-net-01 \
+    --name redis redis:alpine
+
+docker service create --replicas 1 \
+    --network my-net-01 \
+    --name worker ibanez/worker:1.06
+
+docker service create --mode global \
+    --network my-net-01 \
+    --name frontend ibanez/frontend:0.01a
+
+docker service create --replicas 1 \
+    --network my-net-01 \
+    --publish 80:8000/tcp \
+    --name fe \
+    ibanez/frontend:0.01a
+
+
+#######
 
 docker service inspect --pretty helloworld
 docker service tasks helloworld
---
+++ docker/proj05/frontend/build.sh
@@ -0,0 +1,3 @@
+docker build -t ibanez/frontend:0.01a .
+docker login
+docker push ibanez/frontend:0.01a
