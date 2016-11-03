d8bd8f5c4c36baac15e86f4d0910b411635e2d8d
José Ibañez <ibanez.j@gmail.com>
2016-11-03 18:35:58 +0100

    env

--- flask/proj04-Dashboard/probes/install/rest-samples.sh
+++ flask/proj04-Dashboard/probes/install/rest-samples.sh
@@ -3,7 +3,7 @@
 stg="/tmp/stg/"
 
 
-aws s3 cp s3://kumo.fibratel.es/dev01/hc.sample.tgz.gpg /tmp/
+aws s3 cp s3://$aws_bucket/dev01/hc.sample.tgz.gpg /tmp/
 
 gpg --passphrase-file ~/.gnupg/testing.pass --output /tmp/hc.sample.tgz -d /tmp/hc.sample.tgz.gpg
 
--- flask/proj04-Dashboard/probes/install/save-samples.sh
+++ flask/proj04-Dashboard/probes/install/save-samples.sh
@@ -12,12 +12,12 @@ gpg --passphrase-file ~/.gnupg/testing.pass --output /mnt/tmp/stg/2016-06.tgz.gp
 
 
 
-aws s3 cp /tmp/hc.sample.tgz.gpg s3://kumo.fibratel.es/dev01/
+aws s3 cp /tmp/hc.sample.tgz.gpg s3://$aws_bucket/dev01/
 
-aws s3 cp /mnt/tmp/stg/2016-09.tgz.gpg s3://kumo.fibratel.es/dev01/
-aws s3 cp /mnt/tmp/stg/2016-08.tgz.gpg s3://kumo.fibratel.es/dev01/
-aws s3 cp /mnt/tmp/stg/2016-07.tgz.gpg s3://kumo.fibratel.es/dev01/
-aws s3 cp /mnt/tmp/stg/2016-06.tgz.gpg s3://kumo.fibratel.es/dev01/
+aws s3 cp /mnt/tmp/stg/2016-09.tgz.gpg s3://$aws_bucket/dev01/
+aws s3 cp /mnt/tmp/stg/2016-08.tgz.gpg s3://$aws_bucket/dev01/
+aws s3 cp /mnt/tmp/stg/2016-07.tgz.gpg s3://$aws_bucket/dev01/
+aws s3 cp /mnt/tmp/stg/2016-06.tgz.gpg s3://$aws_bucket/dev01/
 
 
 
