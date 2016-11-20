b815758b16713cf1f349cbfcf4797701c3a8fb02
José Ibañez <ibanez.j@gmail.com>
2016-11-20 21:35:07 +0100

    no memory

--- IOU/Proj03/lab1/start-28022.sh
+++ IOU/Proj03/lab1/start-28022.sh
@@ -6,11 +6,11 @@ $IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-ipbasek9-15.1g.bin -p 2001 -
 sleep 5
 
 
-$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-adventerprisek9-15.6.0.9S.bin -p 2011 -- -e4 -s0 11 &
+$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-ipbasek9-15.1g.bin -p 2011 -- -e4 -s0 11 &
 sleep 5
-$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-adventerprisek9-15.6.0.9S.bin -p 2012 -- -e4 -s0 12 &
+$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-ipbasek9-15.1g.bin -p 2012 -- -e4 -s0 12 &
 sleep 5
-$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-adventerprisek9-15.6.0.9S.bin -p 2021 -- -e4 -s0 21 &
+$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-ipbasek9-15.1g.bin -p 2021 -- -e4 -s0 21 &
 sleep 5
-$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-adventerprisek9-15.6.0.9S.bin -p 2022 -- -e4 -s0 22 &
+$IOU/scripts/wrapper.bin -m $IOU/bin/i86bi-linux-l2-ipbasek9-15.1g.bin -p 2022 -- -e4 -s0 22 &
 sleep 5
