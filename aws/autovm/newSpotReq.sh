75d73ee2ca86eae08f89fdea52466a1c0d8b1b29
José Ibañez <ibanez.j@gmail.com>
2017-03-05 21:04:31 +0100

    autovm

--- aws/autovm/newSpotReq.sh
+++ aws/autovm/newSpotReq.sh
@@ -25,8 +25,10 @@ request-spot-instances \
 --spot-price "$price" \
 --instance-count 1 \
 --type "one-time" \
---launch-specification file:///tmp/myVm.json
+--launch-specification file:///tmp/myVM.json
 
 
 #aws ec2 describe-instances --filters "Name=spot-instance-request-id,Values=sir-vv6gh3hn" | jq -r ".Reservations[] | .Instances[] | .InstanceId"
 #aws ec2 create-tags --resources "sir-vv6gh3hn" --tags "Key=Name,Value=kumo-m1"
+
+mv /tmp/myVM.json /tmp/myVM.bak
