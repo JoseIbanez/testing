16f76221a3e544d9af0dffe3d56f0b5c1cc3ae88
José Ibañez <ibanez.j@gmail.com>
2017-08-31 20:47:06 +0200

    cron

--- aws/dynamoDB/p02-graph/deploy.sh
+++ aws/dynamoDB/p02-graph/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pip install boto3 TwitterAPI
 sudo pip install --upgrade requests
-  
+sudo apt-get install -y gnuplot  
--- aws/dynamoDB/p02-graph/graph.sh
+++ aws/dynamoDB/p02-graph/graph.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+home="/home/ubuntu/Projects/testing/aws/dynamoDB/p02-graph"
+cd $home
+
 ./get_temp.py > ./sample.csv
 gnuplot ./g1.plot
 ./tw_upload.py -f temp.png -t 4C
