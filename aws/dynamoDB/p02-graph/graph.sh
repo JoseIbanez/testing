a66f66d186210328347098255d6f1b4d4f641407
José Ibañez <ibanez.j@gmail.com>
2017-09-04 19:03:42 +0200

    19250

--- aws/dynamoDB/p02-graph/graph.sh
+++ aws/dynamoDB/p02-graph/graph.sh
@@ -3,6 +3,11 @@
 home="/home/ubuntu/Projects/testing/aws/dynamoDB/p02-graph"
 cd $home
 
-./get_temp.py > ./sample.csv
+./get_temp.py  > ./sample.csv
 gnuplot ./g1.plot
-./tw_upload.py -f temp.png -t 4C
+./tw_upload.py -f temp.png -t 28039
+
+./get_temp.py -probe b827eb.61eb84.c3  > ./sample.csv
+gnuplot ./g1.plot
+./tw_upload.py -f temp.png -t 19250 
+
