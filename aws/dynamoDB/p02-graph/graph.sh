26847a17be05e295cde60827981ee2b3f0092955
José Ibañez <ibanez.j@gmail.com>
2017-09-17 21:58:44 +0200

    Probe RP z

--- aws/dynamoDB/p02-graph/graph.sh
+++ aws/dynamoDB/p02-graph/graph.sh
@@ -3,7 +3,7 @@
 home="/home/ubuntu/Projects/testing/aws/dynamoDB/p02-graph"
 cd $home
 
-./get_temp.py  > ./sample.csv
+./get_temp.py -probe b827eb.300520.c2 > ./sample.csv
 gnuplot ./g1.plot
 ./tw_upload.py -f temp.png -t 28039
 
