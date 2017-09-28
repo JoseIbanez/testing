0e46b87226a02f91eeb3443db1c0b25788379dd9
José Ibañez <ibanez.j@gmail.com>
2017-09-28 22:36:20 +0200

    tf

--- terraform/p01/test1.tf
+++ terraform/p01/test1.tf
@@ -2,9 +2,25 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web-1" {
   ami           = "ami-1e339e71"
   instance_type = "t2.micro"
   key_name      = "kumo"
   security_groups = ["mgmt"]
+
+  tags {
+    Name = "web-2"
+  }
+}
+
+resource "aws_instance" "web-2" {
+  ami           = "ami-1e339e71"
+  instance_type = "t2.micro"
+  key_name      = "kumo"
+  security_groups = ["mgmt"]
+
+  tags {
+    Name = "web-2"
+  }
+
 }
