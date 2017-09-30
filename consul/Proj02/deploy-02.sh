95a5787800cea40ba8f8868363e9856398a4e335
José Ibañez <ibanez.j@gmail.com>
2017-09-30 11:32:07 +0200

    consul

---
+++ consul/Proj02/deploy-02.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#lxc profile create puppet-node
+#lxc profile set puppet-node user.user-data - < ./config/my-cloud-config.yml
+#lxc profile show puppet-node
+
+lxc network create br0 ipv6.address=none ipv4.address=10.0.3.1/24 ipv4.nat=true
+lxc network attach-profile br0 default
+
+
+lxc launch ubuntu:14.04 u10 -p default
+lxc launch ubuntu:14.04 u11 -p default
+
+lxc launch images:centos/7/amd64   u20 -p default
+lxc launch images:centos/6/amd64   u21 -p default
+
+lxc list
+
+lxc exec u20 bash
+lxc exec u10 /bin/bash
+
+
+for i in `seq 40 69`;
+do
+  sudo lxc launch ubuntu:14.04 u$i -p default
+done
---
+++ consul/Proj02/puppet/init.pp
@@ -0,0 +1,31 @@
+node default {
+   class { 'helloworld': }
+   class { 'helloworld::motd': }
+}
+
+
+class helloworld {
+   notify { 'hello, world!': }
+}
+
+class helloworld::motd {
+
+   file { '/etc/motd':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "hello, world!\n",
+    }
+
+}
+
+class helloworld::packages {
+
+  # you can use a global package parameter
+  Package { ensure => 'installed' }
+
+  package { 'screen': }
+  package { 'nmap': }
+  package { 'sudo': }
+
+}
