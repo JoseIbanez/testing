97b39009e9b3acb8d400d6f3aecdaa6f63eb6e5b
root <root@ip-172-31-20-132.eu-central-1.compute.internal>
2017-10-03 18:58:13 +0000

    init

--- consul/Proj02/puppet/init.pp
+++ consul/Proj02/puppet/init.pp
@@ -1,34 +1,111 @@
-node default {
-    class { 'helloworld': }
-    class { 'helloworld::motd': }
+class consul {
+    notify { 'hello, consul!': }
+
+    file { '/var/consul':
+        ensure => directory,
+        mode => '0755',
+        owner => 'root',
+        group => 'root',
+    }
+
+    package { 'unzip':
+	ensure => 'installed', 
+    }
+
+
+    archive { 'consul_0.9.3_linux_amd64.zip':
+        path          => '/tmp/consul_0.9.3_linux_amd64.zip',
+        source        => 'https://releases.hashicorp.com/consul/0.9.3/consul_0.9.3_linux_amd64.zip',
+        #checksum      => 'f2aaf16f5e421b97513c502c03c117fab6569076',
+        #checksum_type => 'sha1',
+        extract       => true,
+        extract_path  => '/tmp/',
+        #creates       => $install_path,
+        #cleanup       => 'true',
+        cleanup       => 'false',        
+        #require       => File[$install_path],
+        require        => Package['unzip'],
+    }
+
+    file { '/usr/bin/consul':
+        ensure    => 'file',
+        source    => '/tmp/consul',
+    }
+
+
 }
 
-node /^u1.\.lxd$/ {
-    class { 'helloworld::packages': }
-}
 
-class helloworld {
-   notify { 'hello, world!': }
-}
+class consul::server {
 
-class helloworld::motd {
+    notify { 'hello, consul server!': }
 
-   file { '/etc/motd':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "hello, world!\n",
+    file { '/etc/consul.d/server':
+        ensure => directory,
+        mode => '0755',
+        owner => 'root',
+        group => 'root',
+    }
+
+    file { "/etc/consul.d/server/server.json":
+        mode => "0644",
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/consul/server.json',
+    }
+
+
+
+
+    file { "/etc/systemd/system/consul.service":
+        mode => "0644",
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/consul/server.service',
     }
 
 }
 
-class helloworld::packages {
+class consul::bootstrap {
 
-  # you can use a global package parameter
-  Package { ensure => 'installed' }
+    notify { 'hello, consul bootstrap!': }
 
-  package { 'screen': }
-  package { 'nmap': }
-  package { 'sudo': }
+    file { '/etc/consul.d/bootstrap':
+        ensure => directory,
+        mode => '0755',
+        owner => 'root',
+        group => 'root',
+        require => File['/etc/consul.d'],
+    }
+
+    file { '/etc/consul.d':
+        ensure => directory,
+        mode => '0755',
+        owner => 'root',
+        group => 'root',
+    }
+
+
+    file { "/etc/consul.d/bootstrap/bootstrap.json":
+        mode => "0644",
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/consul/bootstrap.json',
+    }
+
+    file { "/etc/systemd/system/consul.service":
+        mode => "0644",
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/consul/bootstrap.service',
+    }
+
+    service { 'consul':
+        ensure => 'running',
+        require => [ File['/etc/consul.d/bootstrap/bootstrap.json'],
+                     File['/etc/systemd/system/consul.service'],
+                     Class['consul'] ],
+    }
+
 
 }
