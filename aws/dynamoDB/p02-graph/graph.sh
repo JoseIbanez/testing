306f763eff597774f3568ac95129898ce1494369
vlan200 <vlan200@live.com>
2017-10-27 20:47:53 +0000

    Siguenza Probe

--- aws/dynamoDB/p02-graph/graph.sh
+++ aws/dynamoDB/p02-graph/graph.sh
@@ -3,7 +3,7 @@
 home="/home/ubuntu/Projects/testing/aws/dynamoDB/p02-graph"
 cd $home
 
-./get_temp.py -probe b827eb.300520.c2 > ./sample.csv
+./get_temp.py -probe b827eb.300520.c3 > ./sample.csv
 gnuplot ./g1.plot
 ./tw_upload.py -f temp.png -t 19250
 
