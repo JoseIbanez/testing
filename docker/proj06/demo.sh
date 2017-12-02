ee8b46543b7b5d593c30723f9c67122adb12d0f8
José Ibañez <ibanez.j@gmail.com>
2017-12-02 23:02:20 +0100

    docker name

--- docker/proj06/demo.sh
+++ docker/proj06/demo.sh
@@ -7,6 +7,13 @@ docker run --env-file ./var.env  python:2.7-alpine sh -c export
 ./make.sh
 docker images
 
-docker run --env-file ./var.env  boto3 sh -c export
+docker tag sqstest $DOCKER_ID_USER/sqstest
+docker push $DOCKER_ID_USER/sqstest
 
-docker run --env-file ./var.env -it  boto3 sh 
+
+docker run --env-file ./var.env  sqstest sh -c export
+
+docker run --env-file ./var.env -it sqstest sh 
+
+docker run -d --env-file ./var.env sqstest /home/producer.py 
+docker run -d --env-file ./var.env sqstest /home/receiver.py 
