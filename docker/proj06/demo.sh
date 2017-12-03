9deb1d35b7090697e5d71f1a8bff46fc637b6efd
José Ibañez <ibanez.j@gmail.com>
2017-12-03 20:55:15 +0100

    consul testing, I found a big issue with NAT

--- docker/proj06/demo.sh
+++ docker/proj06/demo.sh
@@ -13,7 +13,35 @@ docker push $DOCKER_ID_USER/sqstest
 
 docker run --env-file ./var.env  sqstest sh -c export
 
-docker run --env-file ./var.env -it sqstest sh 
+docker run --env-file ./var.env -it ibanez/sqstest sh 
 
 docker run -d --env-file ./var.env sqstest /home/producer.py 
 docker run -d --env-file ./var.env sqstest /home/receiver.py 
+
+
+#swarn
+
+docker swarm init --advertise-addr 172.20.20.11
+
+docker swarm join --token SWMTKN-1-091ggopypyk63wh7ue3stotmse5faz5wikxpeffukp967i3b1g-a1zqb9tw87omf7yds6ykri85j 172.20.20.11:2377
+
+docker service create \
+            --mode replicated \
+            --replicas 5 \
+            --name producer \
+            --env-file ./var.env \
+            ibanez/sqstest /home/producer.py
+
+docker service ps producer
+docker service rm producer
+
+
+docker service create \
+            --mode replicated \
+            --replicas 5 \
+            --name receiver \
+            --env-file ./var.env \
+            ibanez/sqstest /home/receiver.py
+
+docker service ps receiver
+docker service rm receiver
