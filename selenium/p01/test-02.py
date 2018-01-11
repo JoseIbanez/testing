ff80219c7b16879006700c0dc47fe281cf1f4873
José Ibañez (VF) <jibanezw@corp.vodafone.es>
2018-01-11 13:04:37 +0100

    initial test

---
+++ selenium/p01/test-02.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+
+#driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome()
+
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
