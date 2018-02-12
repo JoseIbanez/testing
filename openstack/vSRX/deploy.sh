e627a5cc5ede2c0307a2146994e7e56f6be7eefc
Jose Ibanez <ibanez.j@gmail.com>
2018-02-12 23:56:35 +0100

    vSRX is workign

--- openstack/vSRX/deploy.sh
+++ openstack/vSRX/deploy.sh
@@ -8,8 +8,19 @@ sudo /usr/local/opt/openvpn/sbin/openvpn --config ~/.secrets/fra1.ovpn
 dmesg | grep -i kvm
 
 
+https://www.juniper.net/documentation/en_US/vsrx/topics/task/multi-task/security-vsrx-with-kvm-installing.html#jd0e230
 
-virt-install --name test --memory 1024 --disk /var/lib/glance/images/6419662b-3063-4f7c-a085-9964f6b8fa55 --import
+
+cat /sys/module/kvm_intel/parameters/nested 
+
+sudo rmmod kvm-intel
+sudo sh -c "echo 'options kvm-intel nested=y enable_apicv=n pml=n' >> /etc/modprobe.d/dist.conf"
+sudo modprobe kvm-intel
+
+
+virt-install --name test --memory 1024 \
+        --graphics vnc,listen=0.0.0.0 --noautoconsole \
+        --disk /var/lib/libvirt/images/c1.img --import
 
 
 virsh destroy vSRX
@@ -18,12 +29,28 @@ virsh undefine vSRX
 virt-install --name vSRX --memory 4096 --cpu SandyBridge,+vmx,-invtsc --vcpus=2 \
         --arch=x86_64 \
         --graphics vnc,listen=0.0.0.0 --noautoconsole \
-        --disk path=/tmp/media-srx-ffp-vsrx-vmdisk-15.1X49-D15.4.qcow2.old,size=16,device=disk,bus=ide,format=qcow2 \
+        --disk path=/var/lib/libvirt/images/j1.qcow2,size=16,device=disk,bus=ide,format=qcow2 \
         --os-type linux --os-variant rhel7 --import
 
 
+virt-install --name vSRX --memory 4096 --cpu Westmere,+vmx,-invtsc --vcpus=2 \
+        --arch=x86_64 \
+        --graphics vnc,listen=0.0.0.0 --noautoconsole \
+        --disk path=/var/lib/libvirt/images/j1.qcow2,size=16,device=disk,bus=ide,format=qcow2 \
+        --os-type linux --os-variant rhel7 --import
 
-cat <<EOF> /etc/libvirt/qemu/networks/default.xml
+
+  <cpu mode='custom' match='exact' check='partial'>
+    <model fallback='allow'>SandyBridge</model>
+    <vendor>Intel</vendor>
+    <feature policy='require' name='vmx'/>
+    <feature policy='disable' name='x2apic'/>
+    <feature policy='disable' name='invtsc'/>
+  </cpu>
+  <graphics type='vnc' port='-1' autoport='yes'  listen='127.0.0.1' passwd='1234'/>
+
+
+cat <<EOF > /etc/libvirt/qemu/networks/default.xml
 <network>
 <name>default</name>
 <forward mode='nat'/>
@@ -45,7 +72,7 @@ virsh net-autostart default
 
 
 
-cat <<EOF> /etc/libvirt/qemu/networks/mgmt.xml
+cat <<EOF > /etc/libvirt/qemu/networks/mgmt.xml
 <network>
 <name>mgmt</name>
 <forward mode='nat'/>
@@ -61,7 +88,30 @@ cat <<EOF> /etc/libvirt/qemu/networks/mgmt.xml
 </network>
 EOF
 
+
+cat <<EOF > /etc/libvirt/qemu/networks/mgmt.xml
+<network>
+<name>mgmt</name>
+<forward mode='route'/>
+<bridge name='virbr1' stp='on' delay='0' />
+<ip address='10.39.10.1' netmask='255.255.255.0'>
+<dhcp>
+<range start='10.39.10.50' end='10.39.10.254' />
+</dhcp>
+</ip>
+</network>
+EOF
+
+
+
 virsh net-define /etc/libvirt/qemu/networks/mgmt.xml
 virsh net-start mgmt
 virsh net-autostart mgmt
 
+
+virsh list
+virsh destroy vSRX
+virsh update-device vSRX change-pwd.xml 
+virsh dumpxml --security-info vSRX | grep grap
+virsh start vSRX
+virsh dumpxml --security-info vSRX | grep grap
