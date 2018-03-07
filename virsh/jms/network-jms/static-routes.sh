24630f13d1f2b71d815ebf4ee6d3248db0f916b2
José Ibañez (VF) <jibanezw@corp.vodafone.es>
2018-03-07 11:13:03 +0100

    merge

--- virsh/jms/network-jms/static-routes.sh
+++ virsh/jms/network-jms/static-routes.sh
@@ -5,3 +5,8 @@ auto eth0
 iface eth0 inet static
       . . . 
       up route add -net 10.10.201.0 netmask 255.255.255.0 gw 10.10.20.1
+
+
+
+route add -net 172.30.20.0/24 gw 10.11.1.1
+route add -net 10.64.102.0/23 gw 10.11.1.1
