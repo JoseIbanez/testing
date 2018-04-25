8a4ce32b9d92642566a59b28751c09da7fd8141e
Jose Ibanez <ibanez.j@gmail.com>
2018-04-25 23:18:13 +0200

    express

---
+++ nodejs/node-express/index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const dishRouter = require('./routes/dishRouter');
+
+
+//const hostname = 'localhost';
+const hostname = '0.0.0.0';
+
+
+const port = 3000;
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
--- timeLapse/encode.sh
+++ timeLapse/encode.sh
@@ -1,5 +1,18 @@
+------------------------------
+
 ffmpeg -r 24 \
     -f image2 -s 1920x1080 \
     -start_number 1162 -i Y009%04d.jpg  \
     -vcodec libx264 -crf 25  -pix_fmt yuv420p \
     ~/Movies/tl-20180402.10fps.mp4
+
+
+-------------------
+
+# 20 fps, NikonA300 folder
+
+ffmpeg -r 20 \
+    -f image2 -s 1920x1080 \
+    -start_number 4101 -i DSCN%04d.jpg  \
+    -vcodec libx264 -crf 25  -pix_fmt yuv420p \
+    ~/Movies/tl-20180424.20fps.mp4
