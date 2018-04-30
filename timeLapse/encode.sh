f25bf14c12d6fb30eb71a2ae7ae5ca51941a7784
Jose Ibanez <ibanez.j@gmail.com>
2018-04-30 21:23:47 +0200

    conFusion with mongoose

--- timeLapse/encode.sh
+++ timeLapse/encode.sh
@@ -9,10 +9,22 @@ ffmpeg -r 24 \
 
 -------------------
 
+ffmpeg -r 20 \
+    -f image2 -s 1920x1080 \
+    -start_number 0888 \
+    -i Y014%04d.jpg  \
+    -vcodec libx264 -crf 25  -pix_fmt yuv420p \
+    ~/Movies/tl-20180428.20fps.mp4
+
+
+-------------------
+
+
+
 # 20 fps, NikonA300 folder
 
 ffmpeg -r 20 \
     -f image2 -s 1920x1080 \
     -start_number 4101 -i DSCN%04d.jpg  \
     -vcodec libx264 -crf 25  -pix_fmt yuv420p \
-    ~/Movies/tl-20180424.20fps.mp4
+    ~/Movies/tl-20180429.20fps.mp4
