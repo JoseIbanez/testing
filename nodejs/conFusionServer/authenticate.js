3c033b8222a554f4709e6d54db5d91d5e2a43d20
Jose Ibanez <ibanez.j@gmail.com>
2018-05-05 20:42:36 +0200

    passport

--- nodejs/conFusionServer/authenticate.js
+++ nodejs/conFusionServer/authenticate.js
@@ -3,7 +3,5 @@ var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
