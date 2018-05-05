6b7375949c2424e7d61144531ce4a438dc8ec497
Jose Ibanez <ibanez.j@gmail.com>
2018-05-05 19:37:53 +0200

    passport

--- nodejs/conFusionServer/models/user.js
+++ nodejs/conFusionServer/models/user.js
@@ -1,21 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+User.plugin(passsportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
 
