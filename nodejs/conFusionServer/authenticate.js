f1432dba9778085e91dd7c4021e6385950c3b234
Jose Ibanez <ibanez.j@gmail.com>
2018-05-07 21:36:12 +0200

    assigment 3

--- nodejs/conFusionServer/authenticate.js
+++ nodejs/conFusionServer/authenticate.js
@@ -40,3 +40,15 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function (req, res, next) {
+    console.log("Is this user " + req.user.username + ", admin?: " + req.user.admin);
+  
+    if (!req.user.admin) {
+        err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+  
+    next();
+};
