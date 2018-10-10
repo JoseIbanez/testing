ac654f679d54ec7a1f678875750366d699ee6be7
Jose Ibanez <ibanez.j@gmail.com>
2018-10-10 22:43:21 +0200

    slack

---
+++ timeLapse/followNumbers.py
@@ -0,0 +1,24 @@
+#!/usr/lib/python
+
+
+import sys
+import os
+import string
+
+
+
+index = 1000
+
+
+for line in sys.stdin:
+
+    src = line.strip('\n')
+
+    dst = 'pic-{:04d}.jpg'.format(index)
+    index = index + 1
+
+    os.symlink(src, dst)
+    #print src +" "+ dst
+
+
+
