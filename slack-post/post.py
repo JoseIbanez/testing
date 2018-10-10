bfa384c363b061d25a87ed1ea5409d6e5bbb51fa
Jose Ibanez <ibanez.j@gmail.com>
2018-10-10 22:43:21 +0200

    slack

---
+++ slack-post/post.py
@@ -0,0 +1,20 @@
+import os
+from slackclient import SlackClient
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
+
+#print sc.api_call(
+#    "channels.list",
+#    exclude_archived=1
+#)
+
+sc.api_call(
+    "chat.postMessage",
+    channel="varios",
+    text="Hello from Python! :tada:",
+    attachments=[{"pretext": "hi" ,
+                  "text": "long *text* here\n line 2\n line3"}]
+
+)
+
