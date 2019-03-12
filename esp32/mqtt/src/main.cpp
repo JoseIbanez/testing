9fe6517254062b7155cf9fab033647d09879b6f3
Jose Ibanez <ibanez.j@gmail.com>
2019-03-12 22:42:26 +0100

    lorin32

--- esp32/mqtt/src/main.cpp
+++ esp32/mqtt/src/main.cpp
@@ -19,7 +19,7 @@
 // Add your MQTT Broker IP address, example:
 //const char* mqtt_server = "192.168.1.144";
 //const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
-#define MQTT_SERVER      "192.168.1.30"
+#define MQTT_SERVER      "192.168.1.19"
 #define MQTT_PORT        1883
 #define MQTT_CLIENT_ID   "ESP"
 #define MQTT_TOPIC_OUT   "output"
