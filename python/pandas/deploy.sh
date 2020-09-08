39fbc30fe4b6c19782dfb8e150a45b450e7f342e
Jose Ibanez <ibanez.j@gmail.com>
2020-09-08 21:39:02 +0200

    pandas covid

---
+++ python/pandas/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt install default-jre
+apt install python3.7-minimal 
+python3.7 -m pip install pip
+python3.7 -m pip install tabula-py
+
+url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_123_COVID-19.pdf"
+
