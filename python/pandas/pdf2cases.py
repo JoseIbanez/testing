8a1b7d2809df77a800fb7f7ff062ca1c6d213493
Jose Ibanez <ibanez.j@gmail.com>
2020-09-12 17:27:09 +0200

    filename

--- python/pandas/pdf2cases.py
+++ python/pandas/pdf2cases.py
@@ -1,44 +1,51 @@
 #!/usr/bin/python3.7
 import tabula
+import sys
 
-def processCasesTable(index,filename):
+def processCasesTable(filename):
 
     # Read pdf into list of DataFrame
     df = tabula.read_pdf(f"./data/{filename}", pages='1')
 
     conf=df[0]
+    col0 = conf.columns[0]
+    print("col0:"+col0)
+    if 'SECRETARÍA' in col0:
+        conf=df[1]
+    print(conf.columns)
+
+
     conf.drop([0,1,2,3],inplace=True)
-    len(conf.columns)
-    conf.columns=["ccaa","ct","c1","c14","c7","d14","d7"]
+    conf.columns=["region_name","ct","c1","c14","c7","d14","d7"]
 
     ccaa = conf.iloc[:, 0]
-    ccaa.columns=['ccaa']
+    ccaa.columns=['region_name']
 
     casos_t = conf.iloc[:, 1].str.replace(".","").astype(int)
     casos_1d = conf.iloc[:, 2].str.replace(".","").astype(int)
 
-    conf["casos_t"]=casos_t
-    conf["casos_1d"]=casos_1d
+    conf["cases_total"]=casos_t
+    conf["cases_1day"]=casos_1d
 
     c14 = conf.iloc[:, 3].str.split(" ",n=1,expand=True)
     casos_14d = c14[0].str.replace(".","").astype(int)
     iac_14d = c14[1].str.replace(",",".").astype(float)
-    conf["casos_14d"]=casos_14d
-    conf["iac_14d"]=iac_14d
+    conf["cases_14d"]=casos_14d
+    conf["cases_14d_ai"]=iac_14d
 
 
     s14 = conf.iloc[:, 5].str.split(" ",n=1,expand=True)
     sintomas_14d = s14[0].str.replace(".","").astype(int)
     ias_14d = s14[1].str.replace(",",".").astype(float)
-    conf["sintomas_14d"]=sintomas_14d
-    conf["ias_14d"]=ias_14d
+    conf["symptoms_14d"]=sintomas_14d
+    conf["symptoms_14d_ai"]=ias_14d
 
     conf["filename"]=filename
-    conf["index"]=index
     print(conf)
     result = conf.to_json(orient="records")
     print(result)
 
 
 if __name__ == '__main__':
-    pdf=processCasesTable("203","Actualizacion_203_COVID-19.pdf")
+
+    pdf=processCasesTable(sys.argv[1])
---
+++ typescript/src/greeter.js
@@ -0,0 +1,7 @@
+// greeter.ts
+function greeter(person) {
+    return "Hello " + person + "!";
+}
+//const userName = 'Node Hero'
+var userName = 3;
+console.log(greeter(userName));
