59f2307290dee461e3ce6912916a6bb42a53e538
Jose Ibanez <ibanez.j@gmail.com>
2020-09-13 10:37:29 +0200

    explore gcp bigquery

---
+++ python/pandas/bqInsert.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3.7
+
+#https://www.mydataschool.com/tutDatawarehouseBigQuery-2.html
+#https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries#client-libraries-install-python
+
+import json
+import logging
+import os
+import traceback
+from datetime import datetime
+import io
+import re
+#from six import StringIO
+#from six import BytesIO
+
+from google.api_core import retry
+from google.cloud import bigquery
+# from google.cloud import firestore
+# from google.cloud import pubsub_v1
+#from google.cloud import storage
+
+
+PROJECT_ID = 'ibanez-001'
+BQ_DATASET = 'covid19ES_dev'
+BQ_TABLE = 'cases'
+#CS = storage.Client()
+BQ = bigquery.Client()
+job_config = bigquery.LoadJobConfig()
+
+def delete_rows_by_index(index):
+    query = f"""
+            DELETE
+            FROM `{PROJECT_ID}.{BQ_DATASET}.{BQ_TABLE}`
+            WHERE index = {index}
+            """
+    query_job = BQ.query(query)
+    results = query_job.result()  # Waits for job to complete.
+
+
+def insert_rows_into_bigquery(rows, rows_id):
+     #blob = CS.get_bucket(bucket_name).blob(file_name)
+     #row = json.loads(blob.download_as_string())
+     print('rows: ', rows)
+     table = BQ.dataset(BQ_DATASET).table(BQ_TABLE)
+     errors = BQ.insert_rows_json(table,
+                                   json_rows=rows,
+                                   row_ids=row_ids,
+                                   retry=retry.Retry(deadline=30))
+
+     print(errors)
+     if errors != []:
+          raise BigQueryError(errors)
+
+
+if __name__ == "__main__":
+
+     rows = [
+          {"date": "2020-10-01",
+           "index": 205,
+           "region_iso":"CLM"},
+          {"date": "2020-10-01",
+           "index": 205,
+           "region_iso":"ES"},
+     ]
+     row_ids = ["205.CM", "205.ES"]
+
+     #delete_rows_by_index(205)
+     insert_rows_into_bigquery(rows, row_ids)
--- python/pandas/deploy.sh
+++ python/pandas/deploy.sh
@@ -6,10 +6,13 @@ python3.7 -m pip install pip
 python3.7 -m pip install tabula-py
 python3.7 -m pip install luigi
 python3.7 -m pip install pdfminer.six
+python3.7 -m pip install --upgrade google-cloud-bigquery
 
 
+sudo snap install google-cloud-sdk --classicbq
 
 PATH=$PATH:/home/vagrant/.local/bin
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.secrets/ibanez-001-8e1126c08504.json"
 
 
 url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_123_COVID-19.pdf"
