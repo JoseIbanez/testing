77407eac17667b033dbf7a87e3d1f3197254e5d9
Jose Ibanez <ibanez.j@gmail.com>
2020-09-16 19:37:00 +0200

    deploy

--- python/pandas/deploy/deploy.sh
+++ python/pandas/deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt update -y
 apt install -y default-jre
 apt install -y python3.7-minimal python3-pip
 python3.7 -m pip install pip
@@ -8,11 +9,27 @@ python3.7 -m pip install luigi
 python3.7 -m pip install pdfminer.six
 python3.7 -m pip install --upgrade google-cloud-bigquery
 
+#Ubuntu 20.04
+#apt install -y python3.7-minimal 
+apt install -y python3-pip
+python3 -m pip install tabula-py
+python3 -m pip install luigi
+python3 -m pip install pdfminer.six
+python3 -m pip install --upgrade google-cloud-bigquery
 
-sudo snap install google-cloud-sdk --classicbq
+
+sudo snap install luigi-server
+sudo snap install google-cloud-sdk --classic
+
+mkdir -p $HOME/.secrets/
+mkdir -p /etc/luigi
+cp ./luigi.cfg /etc/luigi/
 
 PATH=$PATH:/home/vagrant/.local/bin
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.secrets/ibanez-001-8e1126c08504.json"
+
+
+
 
 
 url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_123_COVID-19.pdf"
