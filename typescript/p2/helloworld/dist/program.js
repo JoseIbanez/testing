64748e872eb11689e59a15a7cc1971b7b48df2b4
Jose Ibanez <ibanez.j@gmail.com>
2020-10-25 20:34:16 +0100

    dist

--- typescript/p2/helloworld/dist/program.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var utils_1 = require("./lib/utils");
-var result1;
-var result2;
-result1 = utils_1.sayHello('Ross');
-result2 = utils_1.sayHello('Peter');
-console.log(result1);
-console.log(result2);
