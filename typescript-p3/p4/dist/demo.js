33ff900a3eb5568fd4977a92ef0fee289fa81bef
Jose Ibanez <ibanez.j@gmail.com>
2020-10-31 10:13:29 +0100

    typescript & npm pkg

---
+++ typescript-p3/p4/dist/demo.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("ibanez-p3/index");
+/////////////
+//let empObj = new Employee("Steve Jobs", 1);
+//empObj.displayEmployee();
+//empObj.empName="Peter P."
+//empObj.displayEmployee();
+console.log(index_1.Greeter('Carl'));
