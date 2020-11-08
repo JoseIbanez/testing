adf17ce60ac05a6a9c4c11f9341ccae5727f2780
Jose Ibanez <jose.ibanez@vodafone.com>
2020-11-08 20:29:41 +0100

    merge

--- typescript/deploy.sh
+++ typescript/deploy.sh
@@ -2,18 +2,13 @@
 
 # https://linuxize.com/post/how-to-install-node-js-on-ubuntu-18.04/
 
-
-#Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.36.0/install.sh | bash
 nvm --version
 
-#Install nodejs
 nvm install node
 node --version
 
-#Install typescript
 npm install -g typescript
-tsc --version
+tsc -v
 
 
