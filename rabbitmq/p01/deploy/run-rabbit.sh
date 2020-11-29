8568af7e5a25c66e5f11d19cadacf168fd679dcc
Jose Ibanez <ibanez.j@gmail.com>
2020-11-29 22:30:16 +0100

    asyncio

---
+++ rabbitmq/p01/deploy/run-rabbit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+grep -q 'redis' /etc/hosts  || echo '127.0.0.1 redis rabbitmq postgres' | sudo tee -a /etc/hosts
+
+sudo docker run -d -p 6379:6379 redis
+
+#docker run -d -p 5672:5672 -p 25672:25672 rabbitmq
+sudo docker run -d -p 5672:5672 -p 15672:15672 rabbitmq:3-management
---
+++ rabbitmq/p01/src/hello.py
@@ -0,0 +1,75 @@
+import asyncio
+import time
+import producer
+import json
+
+p1 = producer.MyProducter()
+
+
+async def fun1(a,b,result):
+    p1.publish("fun1. running")
+    await asyncio.sleep(2)
+    result["c"] = a+b
+    #print(f"fun1 {c}")
+    p1.publish("fun1. done")
+    p1.publish(json.dumps(result))
+
+
+async def fun2(a,b,result):
+    p1.publish("fun2. running")
+    await asyncio.sleep(1)
+    result["d"] = a-b
+    #print(f"fun2 {c}")
+    p1.publish("fun2. done")
+    p1.publish(json.dumps(result))
+
+
+async def funGen(name,time,value,result):
+    p1.publish(f"{name} running")
+    await asyncio.sleep(time)
+    result[name] = value
+    p1.publish(f"{name} done")
+    p1.publish(json.dumps(result))
+
+
+async def main():
+    result = { "c": 0, "d":0 }
+    print('Hello ...')
+    tasks = []
+    tasks.append(fun1(1,2,result))
+    tasks.append(fun2(1,2,result))
+    await asyncio.wait(tasks)
+
+    tasks = []
+    tasks.append(funGen("name",1,"es000yr",result))
+    tasks.append(funGen("ipFE",1,"44.44.44.10",result))
+    tasks.append(funGen("ipMG",1,"10.10.10.10",result))
+    await asyncio.wait(tasks)
+
+    tasks = []
+    tasks.append(funGen("Clone",10,"done",result))
+    tasks.append(funGen("Infoblox",1,"done",result))
+    await asyncio.wait(tasks)
+
+    tasks = []
+    tasks.append(funGen("Route",2,"done",result))
+    tasks.append(funGen("Locale",2,"done",result))
+    tasks.append(funGen("Avamar",2,"done",result))
+    tasks.append(funGen("Zerto",2,"done",result))
+    await asyncio.wait(tasks)
+
+
+    print('... World!')
+
+
+
+
+if __name__ == "__main__":
+    s = time.perf_counter()
+
+    asyncio.run(main())
+
+    elapsed = time.perf_counter() - s
+    print(f"Execution time: {elapsed:0.2f} seconds.")
+
+
