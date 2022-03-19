bb55d1024a011aeb2844c7c92c25aa49ebd15efb
Jose Ibanez <ibanez.j@gmail.com>
2022-03-19 19:38:05 +0000

    acestream

---
+++ acestream/hls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+
+if [ -n "$1" ]; then
+  ID=`echo $1 | sed 's@.*//@@'`
+fi
+
+cd /mnt/d1/hls/3231
+rm -f *.ts
+rm -f *.m3u8
+
+#export ACE_URL="http://127.0.0.1:6878/ace/manifest.m3u8?id=$ID"
+export ACE_URL="http://127.0.0.1:6878/ace/getstream?id=$ID"
+
+ffmpeg \
+ -i $ACE_URL \
+ -c copy -map 0 \
+ -f hls \
+ -hls_list_size 20 -hls_delete_threshold 3 -hls_flags delete_segments \
+ -hls_init_time 4 -hls_time 4 \
+ stream.m3u8
