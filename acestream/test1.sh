038d83cdc72550d432b3c4d18a0d2242a8efde8a
Jose Ibanez <ibanez.j@gmail.com>
2022-09-25 18:22:24 +0000

    fix(acestream): some fixes

--- acestream/test1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-https://hub.docker.com/r/bfayers/docker-acestream-proxy
-
-docker run -d -p 8000:8000 -e "UPLOAD_LIMIT=2000" -e "DOWNLOAD_LIMIT=8000" --name aceproxy bfayers/docker-acestream-proxy
-
-vlc http://127.0.0.1:8000/pid/STREAMID/stream.mp4
