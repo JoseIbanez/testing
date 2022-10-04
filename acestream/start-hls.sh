83ca57156da0360ee0d6efb86f3c12048a2a1334
Jose Ibanez <ibanez.j@gmail.com>
2022-10-04 07:50:01 +0000

    fix(acestream): start and test

--- acestream/start-hls.sh
+++ acestream/start-hls.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
 #M. LaLiga 1080 multi audio
-/home/ibanez/.local/bin/hls acestream://167e3b44a520cd76d4372f6d30fe6d7ccd524175
-sleep 1
+#sleep 1; /home/ibanez/.local/bin/hls acestream://167e3b44a520cd76d4372f6d30fe6d7ccd524175
+
+
+#DAZN LaLiga 1080 MultiAudio
+sleep 1; /home/ibanez/.local/bin/hls acestream://df98650743f24a245c44cdf2851e57078f4c487a 3232
+
+
+
+
+#DAZN 1 1080 
+sleep 1; /home/ibanez/.local/bin/hls acestream://d4d4200a1e52fc23940b2dde915600af7e9a6b05 3241
+
+# Sky Sports Main event
+#sleep 1; /home/ibanez/.local/bin/hls eab7aeef0218ce8b0752e596e4792b69eda4df5e 3242
+
+# BT Sports 1
+#sleep 1; /home/ibanez/.local/bin/hls efc60cfe5e3a349baa02bcc49f6647c21a9c3c5b 3243
+
+
 
 
 #DAZN F1 1080 (Fórmula 1)
-#/home/ibanez/.local/bin/hls acestream://c372020ed2744033f56548b6beb11a9f3e740f37 3232
+sleep 1; /home/ibanez/.local/bin/hls acestream://c372020ed2744033f56548b6beb11a9f3e740f37 3251
+
 
 
 
