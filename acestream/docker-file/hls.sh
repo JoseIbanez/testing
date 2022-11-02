09ccfadcb136b9692804646bf63df2d59acc2080
Jose Ibanez <ibanez.j@gmail.com>
2022-11-02 16:30:33 +0000

    fix(acestream): docker image

---
+++ acestream/docker-file/hls.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Acelink ID
+ID=`echo $1 | sed 's@.*//@@'`
+
+
+# Client port
+if [ -n "$2" ]; then
+  PORT=$2
+else
+  PORT="3200"
+fi
+echo "Client Port: $PORT"
+
+
+# Peer port
+if [ -n "$3" ]; then
+  PEERPORT="-p 8621:8621"
+else
+  PEERPORT=""
+fi
+
+
+#######
+mkdir -p /mnt/d1/hls/$PORT
+mkdir -p /mnt/d1/hls-log/$PORT
+
+docker kill "acelink.$PORT" | echo ""
+sleep 3
+
+docker run -d --rm  \
+    -v /mnt/d1/hls/$PORT:/mnt/hls \
+    -e "ID=$ID"  \
+    --name "acelink.$PORT" \
+    -p $PORT:6878 $PEERPORT \
+    --cap-add=NET_ADMIN \
+    acelink-ffmpeg
+
+cd .. ; ./test-acestream.py -p $PORT
