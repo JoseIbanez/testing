4eae2b83f8c01c96a4a68370ae9509a373097319
Jose Ibanez <ibanez.j@gmail.com>
2023-03-28 16:08:17 +0000

    fix(bot): use acelink api

--- telegram/bot1/bot.py
+++ telegram/bot1/bot.py
@@ -3,11 +3,17 @@
 import logging
 import os
 import time
+import json
 import shlex
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-import subprocess
+import requests
+from requests.exceptions import HTTPError,ConnectionError
+
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+ACELINKHOST = os.environ.get("ACELINKHOST","http://localhost:8008")
+ACELINKTOKEN = os.environ.get("ACELINKTOKEN","MAGIC")
 
 
 logging.basicConfig(
@@ -15,10 +21,11 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a super bot, please talk to me!")
+
+
 
 async def hls(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id=update.effective_chat.id
@@ -34,17 +41,63 @@ async def hls(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await context.bot.send_message(chat_id, text=f"{user}, wait a second for AceId:{ace_id}, Port:{port}")
 
-    try:
-        result = subprocess.run(["hls", ace_id, port], stderr=subprocess.PIPE, text=True)
-    except FileNotFoundError as e:
+    try:        
+        path = f"{ACELINKHOST}/hls/{port}/"
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': f"Bearer {ACELINKTOKEN}"}
+        data = {
+            'ace_id': ace_id,
+            'description': ''
+        }
+        result = requests.put(path,headers=headers,json=data)
+
+
+    except (HTTPError, ConnectionError) as e:
         await context.bot.send_message(chat_id, text=f"Something was wrong:\n{e}")
         return
 
-    if result.stderr:
-        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{result.stderr}")
+    if result.status_code > 299:
+        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{result.status_code} {result.text}")
         return
 
+    await context.bot.send_message(chat_id, text=json.dumps(result.json(),indent=2))
+
     await context.bot.send_message(chat_id, text="I'm a super bot, please talk to me!")
+
+
+async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id=update.effective_chat.id
+    cmd = shlex.split(update.message.text, posix=False)
+
+    if len(cmd) < 2:
+        await context.bot.send_message(chat_id, text="wrong cmd, try /check <port>")
+        return
+
+    port=cmd[1]
+    user=update.message.from_user.first_name
+
+    await context.bot.send_message(chat_id, text=f"{user}, wait a second for docker Port:{port}")
+
+    try:
+        
+        path = f"{ACELINKHOST}/hls/{port}/"
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': f"Bearer {ACELINKTOKEN}"}
+        result = requests.get(path,headers=headers)
+
+
+    except (HTTPError, ConnectionError) as e:
+        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{e}")
+        return
+
+    if result.status_code > 299:
+        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{result.status_code} {result.text}")
+        return
+
+    await context.bot.send_message(chat_id, text=json.dumps(result.json(),indent=2))
+
+    await context.bot.send_message(chat_id, text="I'm a super bot, please talk to me!")
+
 
 
 async def kill(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -61,14 +114,22 @@ async def kill(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id, text=f"{user}, wait a second for docker Port:{port}")
 
     try:
-        result = subprocess.run(["docker", "kill", f"acelink.{port}"], stderr=subprocess.PIPE, text=True)
-    except FileNotFoundError as e:
+        
+        path = f"{ACELINKHOST}/hls/{port}/"
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': f"Bearer {ACELINKTOKEN}"}
+        result = requests.delete(path,headers=headers)
+
+
+    except (HTTPError, ConnectionError) as e:
         await context.bot.send_message(chat_id, text=f"Something was wrong:\n{e}")
         return
 
-    if result.stderr:
-        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{result.stderr}")
+    if result.status_code > 299:
+        await context.bot.send_message(chat_id, text=f"Something was wrong:\n{result.status_code} {result.text}")
         return
+
+    await context.bot.send_message(chat_id, text=json.dumps(result.json(),indent=2))
 
     await context.bot.send_message(chat_id, text="I'm a super bot, please talk to me!")
 
@@ -82,10 +143,12 @@ if __name__ == '__main__':
     
     start_handler = CommandHandler('start', start)
     hls_handler = CommandHandler('hls', hls)
+    check_handler = CommandHandler('check', check)
     kill_handler = CommandHandler('kill', kill)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
     application.add_handler(hls_handler)
+    application.add_handler(check_handler)
     application.add_handler(kill_handler)
     application.add_handler(echo_handler)
     
