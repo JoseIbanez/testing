296ba24cfd73164082adae76ead8c6d6786473d0
Jose Ibanez <ibanez.j@gmail.com>
2023-04-08 09:55:28 +0200

    draft(stocks): poc

--- golang/scrape_ftv/hello.go
+++ golang/scrape_ftv/hello.go
@@ -6,17 +6,64 @@ import (
 	"net/http"
 	"regexp"
 
+	"path/filepath"
+
 	"github.com/gocolly/colly/v2"
 )
 
 type Event struct {
-	Date          string
-	Championship  string 
-	Local         string
-	Visit         string
-	Channels      string
+	Date         string
+	Championship string
+	Local        string
+	Visit        string
+	Channels     string
 }
 
+func parse_table(e *colly.HTMLElement, eventList []Event) {
+
+	date_list := []string{}
+	date_re := regexp.MustCompile(`(\d+/\d+/\d+)`)
+
+	e.ForEach("tbody > tr", func(_ int, tr *colly.HTMLElement) {
+
+		if tr.Index == 0 {
+			date := tr.ChildText("td:nth-child(1)")
+			match := date_re.FindStringSubmatch(date)
+			if len(match) > 0 {
+				date_list = append(date_list, match[1])
+			}
+			return
+		}
+
+		if len(date_list) < 1 {
+			return
+		}
+
+		cols := len(tr.DOM.Children().Nodes)
+		date := date_list[0]
+		event := Event{}
+		if cols == 5 {
+			event.Date = date + " " + tr.ChildText("td:nth-child(1)")
+			event.Championship = tr.ChildText("td:nth-child(2)")
+			event.Local = tr.ChildText("td:nth-child(3)")
+			event.Visit = tr.ChildText("td:nth-child(4)")
+			event.Championship = tr.ChildText("td:nth-child(5)")
+		} else if cols == 4 {
+			event.Date = date + " " + tr.ChildText("td:nth-child(1)")
+			event.Championship = tr.ChildText("td:nth-child(2)")
+			event.Local = tr.ChildText("td:nth-child(3)")
+			event.Visit = tr.ChildText("td:nth-child(4)")
+			event.Championship = tr.ChildText("td:nth-child(5)")
+		} else {
+			return
+		}
+
+		fmt.Printf("%v\n", event)
+		eventList = append(eventList, event)
+
+	})
+
+}
 
 func main() {
 	// scraping logic...
@@ -27,58 +74,10 @@ func main() {
 	c := colly.NewCollector()
 	c.WithTransport(t)
 
-	eventList = make []EveEvent
+	eventList := make([]Event, 0)
 
 	c.OnHTML("table.tablaPrincipal", func(e *colly.HTMLElement) {
-		//fmt.Println("tablaPrincipal")
-		//fmt.Println(e.Text)
-
-		date_list := []string{}
-		//even_list := []string{}
-
-		date_re := regexp.MustCompile(`(\d+/\d+/\d+)`)
-
-		e.ForEach("tbody > tr", func(_ int, tr *colly.HTMLElement) {
-			//fmt.Println(tr.Text)
-
-			if tr.Index == 0 {
-				date := tr.ChildText("td:nth-child(1)")
-				match := date_re.FindStringSubmatch(date)
-				if len(match) > 0 {
-					date_list = append(date_list, match[1])
-				}
-				return
-			}
-
-			if len(date_list) < 1 {
-				return
-			}
-
-			cols := len(tr.DOM.Children().Nodes)
-			date := date_list[0]
-
-			if cols == 5 {
-				hour := date + " " + tr.ChildText("td:nth-child(1)")
-				championship := tr.ChildText("td:nth-child(2)")
-				//local        := tr.ChildText("td:nth-child(3)")
-				//visit        := tr.ChildText("td:nth-child(4)")
-				//channels     := tr.ChildText("td:nth-child(5)")
-
-				fmt.Println(hour + " " + championship)
-
-			} else if cols == 4 {
-				hour := date + " " + tr.ChildText("td:nth-child(1)")
-				championship := tr.ChildText("td:nth-child(2)")
-				//local        := tr.ChildText("td:nth-child(3)")
-				//visit        := ""
-				//channels     := tr.ChildText("td:nth-child(4)")
-
-				fmt.Println(hour + " " + championship)
-
-			}
-
-		})
-
+		parse_table(e, eventList)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
@@ -87,7 +86,9 @@ func main() {
 
 	fmt.Println("Hello, World!")
 
-	c.Visit("file:///Users/ibanez/Projects/testing/golang/scrape_ftv/sample-date/fetv.html")
+	absPath, _ := filepath.Abs("./sample-date/fetv.html")
+	c.Visit("file://" + absPath)
+
 	c.Wait()
 
 }
