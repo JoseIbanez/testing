ea56c2815f32590b2e7008f502efefc7ca43af86
Jose Ibanez <ibanez.j@gmail.com>
2023-05-09 23:56:47 +0200

    fix(redis): cluster startup

--- redis/k8s-redis-cluster/redis-config/start-cluster.sh
+++ redis/k8s-redis-cluster/redis-config/start-cluster.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
 get_ip() {
-    nslookup $1 | grep -A 2 Name: | grep Address: | cut -d' ' -f2
+    nslookup -type=a $1 | grep -A 2 Name: | grep Address: | cut -d' ' -f2
 }
 
-sleep 10
-BASENAME="redis-cluster-redis-node"
-CLUSTER_SIZE=6
+echo "** start redis cluster config **"
+sleep 20
+BASENAME="redis-cluster"
+DOMAIN=$(cat /etc/resolv.conf | grep search | cut -d' ' -f2)
+MAX_NODE=5
+NODELIST=""
 
-
-for i in $(seq 1 $CLUSTER_SIZE); do
-    NODELIST="${NODELIST} $(get_ip $BASENAME-$i):6379"
+for i in $(seq 0 $MAX_NODE); do
+    HOSTNAME="$BASENAME-$i.$BASENAME.$DOMAIN"
+    ping -c 3 $HOSTNAME
+    NODELIST="${NODELIST} $(get_ip $HOSTNAME):6379"
 done
+echo $NODELIST
 
 set -x
 redis-cli --cluster create $NODELIST --cluster-replicas 1 --cluster-yes
 set +x
 
 while true; do
-    redis-cli --cluster check "redis-node:6379"    
-    redis-cli --cluster info  "redis-node:6379"    
+    redis-cli --cluster check "$BASENAME:6379"
+    redis-cli --cluster info  "$BASENAME:6379"
     sleep 60
 done
