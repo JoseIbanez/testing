ebd65a2af4a7a43185141bdc1a031dfc0c29eff8
Jose Ibanez <jose.ibanez@vodafone.com>
2023-05-10 12:44:50 +0200

    fix(redis): cluster monitor script

--- redis/k8s-redis-cluster/redis-config/start-cluster.sh
+++ redis/k8s-redis-cluster/redis-config/start-cluster.sh
@@ -4,6 +4,53 @@ get_ip() {
     nslookup -type=a $1 | grep -A 2 Name: | grep Address: | cut -d' ' -f2
 }
 
+
+remove_failed_nodes() {
+
+    HOSTNAME=$1
+    MAX_NODE=5
+
+    # Remove failed nodes
+    FAILED=$(redis-cli -c -h $HOSTNAME -p 6379 CLUSTER NODES | grep fail | cut -d' ' -f 1); 
+    echo "Failed nodes: $FAILED"
+    for n in $FAILED; do 
+        echo "Remove failed Node: $n"
+
+        for i in $(seq 0 $MAX_NODE); do
+            NODENAME="$BASENAME-$i.$BASENAME.$DOMAIN"
+            redis-cli -c -h $NODENAME -p 6379 CLUSTER FORGET $n
+
+        done
+
+    done
+
+
+    # Save a valid node 
+    OTHER=$(redis-cli -c -h $HOSTNAME -p 6379 CLUSTER NODES | grep -v slave | grep -v myself | head -n 1 | cut -d' ' -f2 | cut -d '@' -f1)
+    if [ -n "$OTHER" ]; then
+        echo $OTHER > /tmp/other_node
+    fi
+
+}
+
+
+add_new_node() {
+
+    HOSTNAME=$1
+
+    HOSTURL="$(get_ip $HOSTNAME):6379"
+
+    # Add new nodes
+    NEWNODE=$(redis-cli -c -h $HOSTNAME -p 6379 CLUSTER NODES | grep -v myself)
+    OTHER=$(cat /tmp/other_node)
+    if [ -z "$NEWNODE" ] && [ -n "$OTHER" ]; then
+        echo "Adding new node $HOSTURL to current cluster $OTHER"
+        redis-cli --cluster add-node $HOSTURL $OTHER --cluster-slave
+    fi
+
+}
+
+
 echo "** start redis cluster config **"
 sleep 20
 BASENAME="redis-cluster"
@@ -23,7 +70,24 @@ redis-cli --cluster create $NODELIST --cluster-replicas 1 --cluster-yes
 set +x
 
 while true; do
-    redis-cli --cluster check "$BASENAME:6379"
-    redis-cli --cluster info  "$BASENAME:6379"
+
+
+    HOSTNAME="$BASENAME-$(( $RANDOM % $MAX_NODE )).$BASENAME.$DOMAIN"
+    HOSTURL="$(get_ip $HOSTNAME):6379"
+
+    echo 
+    echo "** Hostname:$HOSTNAME IP:$HOSTURL"
+
+    redis-cli -c -h $HOSTNAME -p 6379 CLUSTER NODES
+    redis-cli -c -h $HOSTNAME -p 6379 CLUSTER INFO
+
+    remove_failed_nodes $HOSTNAME
+
+    add_new_node $HOSTNAME 
+
+
+    redis-cli --cluster check $HOSTURL
+    redis-cli --cluster info  $HOSTURL
+
     sleep 60
 done
