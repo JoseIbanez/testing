1fd2cb897a51ca00afc8d7cf28b132a6e02207d5
Jose Ibanez <ibanez.j@gmail.com>
2023-05-17 16:48:31 +0000

    fix(iptv): replay

--- telegram/bot2/stock_price_bot/lib/yfinance_lib.py
+++ telegram/bot2/stock_price_bot/lib/yfinance_lib.py
@@ -5,6 +5,7 @@ import logging
 import time
 import os
 from requests import Session
+from requests.exceptions import HTTPError
 from requests_cache import CacheMixin, SQLiteCache
 from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 from pyrate_limiter import Duration, RequestRate, Limiter
@@ -39,44 +40,54 @@ valList=['ITX.MC', 'ELE.MC', 'EOAN.DE', 'VOW3.DE', 'DTE.DE', 'DBK.DE', 'BAYN.DE'
 
 def get_symbol_info(symbol_id:str) -> Symbol:
 
-    logger.info("Geting infor for %s",symbol_id)
-
-    time0 = time.time()
-    tickerXL=yf.Ticker(symbol_id,session=sessionXL)
-
     try:
+
+        logger.info("Geting infor for %s",symbol_id)
+
+        time0 = time.time()
+        tickerXL=yf.Ticker(symbol_id,session=sessionXL)
+
         if tickerXL.info is None:
             logger.error("Symbol:%s not found",symbol_id)
             return None
+
+        s = Symbol()
+        s.id = symbol_id
+        s.displayName = tickerXL.info.get('displaytName') or tickerXL.info.get('shortName') 
+        s.marketState = tickerXL.info.get('marketState')
+        s.currency = tickerXL.info.get('currency')
+        exchangeTimezoneName = tickerXL.info.get('exchangeTimezoneName')
+
+        s.previousClose = tickerXL.fast_info.get('previousClose')
+        s.lastPrice = tickerXL.fast_info.get('lastPrice')
+
+        ticker = None
+        opening = check_opening(symbol_id,exchangeTimezoneName)
+
+        if s.marketState == "REGULAR" or opening:
+            ticker=yf.Ticker(symbol_id,session=session)
+            s.lastPrice = ticker.fast_info.get('lastPrice')
+        
+        if opening and ticker:
+            s.marketState = ticker.info.get('marketState')
+            s.previousClose = ticker.fast_info.get('previousClose')
+
+
+        s.priceVariation = (s.lastPrice - s.previousClose)/s.previousClose*100
+
+        logger.info("Symbol:%s updated, delay:%.2fsec",symbol_id,time.time()-time0)
+
+    except HTTPError as e:
+        logger.error(e)
+        logger.error("Waiting for rate limit")
+        time.sleep(60)
+        return None
+
     except AttributeError:
         logger.error("Symbol:%s not found",symbol_id)
         return None
 
-    s = Symbol()
-    s.id = symbol_id
-    s.displayName = tickerXL.info.get('displaytName') or tickerXL.info.get('shortName') 
-    s.marketState = tickerXL.info.get('marketState')
-    s.currency = tickerXL.info.get('currency')
-    exchangeTimezoneName = tickerXL.info.get('exchangeTimezoneName')
 
-    s.previousClose = tickerXL.fast_info.get('previousClose')
-    s.lastPrice = tickerXL.fast_info.get('lastPrice')
-
-    ticker = None
-    opening = check_opening(symbol_id,exchangeTimezoneName)
-
-    if s.marketState == "REGULAR" or opening:
-        ticker=yf.Ticker(symbol_id,session=session)
-        s.lastPrice = ticker.fast_info.get('lastPrice')
-    
-    if opening and ticker:
-        s.marketState = ticker.info.get('marketState')
-        s.previousClose = ticker.fast_info.get('previousClose')
-
-
-    s.priceVariation = (s.lastPrice - s.previousClose)/s.previousClose*100
-
-    logger.info("Symbol:%s updated, delay:%.2fsec",symbol_id,time.time()-time0)
 
     return s
 
