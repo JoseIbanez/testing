7a05fdd646a45fd7e06573f8bad77d313f8376a8
Jose Ibanez <ibanez.j@gmail.com>
2023-05-30 10:36:22 +0200

    fix(grpc): error list

--- python/grpc/nginx2/create_certificates.sh
+++ python/grpc/nginx2/create_certificates.sh
@@ -2,8 +2,15 @@
 
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
 
+
 # non-interactive and 10 years expiration
 
 export FILENAME=grpc.example.com.crt
 export SUBJ="/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=h3.local"
 openssl req -x509 -newkey rsa:4096 -keyout $FILENAME.key -out $FILENAME.crt -sha256 -days 3650 -nodes -subj $SUBJ
+
+
+# Install:
+sudo cp $FILENAME.crt   /etc/ssl/certs/
+sudo cp $FILENAME.key   /etc/ssl/private/
+
---
+++ python/grpc/nginx2/install.sh
@@ -0,0 +1,5 @@
+#Install
+
+
+sudo cp errors.grpc_conf /etc/nginx/conf.d/errors.grpc_conf
+sudo cp grpc-site.conf   /etc/nginx/conf.d/site-enables/grpc-site.conf
