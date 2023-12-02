88163f9b2cc691a93e740b8b6f11866422d24b2c
Jose Ibanez <ibanez.j@gmail.com>
2023-12-02 19:09:39 +0100

    fix(web1): test sprintboot

---
+++ java/web1/src/test/java/org/example/web1/Web1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.web1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
