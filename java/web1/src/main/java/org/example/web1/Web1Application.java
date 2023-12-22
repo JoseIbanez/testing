aa49b1e175d1f8b8849e7582cf83910a088d826b
Jose Ibanez <ibanez.j@gmail.com>
2023-12-22 18:05:36 +0100

    fix(java): jdbcTemplate and profile

--- java/web1/src/main/java/org/example/web1/Web1Application.java
+++ java/web1/src/main/java/org/example/web1/Web1Application.java
@@ -1,9 +1,11 @@
 package org.example.web1;
 
+import org.example.web1.repo.SingerRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,11 +15,13 @@ public class Web1Application {
 
 	private static final Logger logger = LoggerFactory.getLogger((Web1Application.class));
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(Web1Application.class, args);
 		logger.info("Hi {} {}","java","!");
 
 	}
+
 
 }
