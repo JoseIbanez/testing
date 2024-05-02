f0e53aa35ab98ad77ef13e0b41fb5ab4bf8aee3d
Jose Ibanez <ibanez.j@gmail.com>
2024-05-02 08:45:26 +0000

    fix(acelink): upgrade to 3.2.3

--- acestream.2/dockerfile-ffmpeg/build.sh
+++ acestream.2/dockerfile-ffmpeg/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t acelink-ffmpeg:3.1.75 -t acelink-ffmpeg:latest  .
+#docker build -t acelink-ffmpeg:3.1.75 -t acelink-ffmpeg:latest  .
+docker build -t acelink-ffmpeg:3.2.3 -t acelink-ffmpeg:latest  .
